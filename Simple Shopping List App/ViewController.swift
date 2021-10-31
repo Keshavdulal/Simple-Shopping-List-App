d11219075971ea803e9e090015df21e816965054
keshavdulal <keshav.dulal@gmail.com>
2021-11-01 00:45:28 +0545

    Initial Commit

---
+++ Simple Shopping List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Shopping List App
+//
+//  Created by Keshav Dulal on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
